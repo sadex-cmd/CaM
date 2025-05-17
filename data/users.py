937b726e10c0625223fe1368b3b41603f366e201
sadex-cmd <obeme2001@gmail.com>
2025-05-17 18:37:45 +0300

    Add files via upload

---
+++ data/users.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=True, unique=True)
+    age = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    def __repr__(self):
+        return self.login
